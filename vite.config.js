122a0aaab4673ff2926a94ba87dc379dfe56661d
LegallyAlive <jarvisgan@gmail.com>
2023-12-09 00:22:19 +0800

    fixed env issues

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,15 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
-import { fileURLToPath } from "url";
+import dotenv from "dotenv";
+
+dotenv.config({path: '.env'});
 
 // https://vitejs.dev/config/
 export default defineConfig({
   // note: we are now running node server.js instead of vite
   plugins: [react()],
+  define: {
+    'process': process
+  }
 });
+ 
