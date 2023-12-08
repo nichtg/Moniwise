d29af157b16293367c824d13d52efcaaee6cd61c
LegallyAlive <jarvisgan@gmail.com>
2023-12-09 01:06:47 +0800

    added a response card and placeholder values for input

--- src/assets/ApiEndpointGenerator.jsx
+++ src/assets/ApiEndpointGenerator.jsx
@@ -15,13 +15,25 @@ export const ApiEndpointGenerator = (apiConfig, onSubmit) => {
   const [form] = Form.useForm();
   const [selectedIndex, setSelectedIndex] = useState(0);
   const [currentFormInput, setCurrentFormInput] = useState("");
+  const [loading, setLoading] = useState(false);
+  const [responseJson, setResponseJson] = useState("");
+
+  const response = {
+    status: 200,
+    statusText: "OK",
+    data: {
+      userID: "60a6b0b9c9b0c2b4b8b0b0b0",
+      note: "test",
+      amount: 100,
+      category: "test",
+    },
+  };
 
   const onFinish = (values) => {
     //get values from formInput
     console.log("currentFormInput: ", currentFormInput);
-    console.log("Form values:", form.getFieldValue(`${currentFormInput}`));
-
-    // onSubmit(values);
+    const fieldValue = JSON.parse(form.getFieldValue(`${currentFormInput}`));
+    setResponseJson(JSON.stringify(response, null, 2));
   };
 
   const onFinishFailed = (errorInfo) => {
@@ -40,7 +52,8 @@ export const ApiEndpointGenerator = (apiConfig, onSubmit) => {
         }}
         defaultActiveKey={selectedIndex}>
         {apiConfig.map((section, index) => {
-          const { input, label, name, rules, description, method } = section;
+          const { input, label, name, rules, description, method, value } =
+            section;
           return (
             <Collapse.Panel key={index} header={`${method}: ${label}`}>
               <Form
@@ -51,12 +64,16 @@ export const ApiEndpointGenerator = (apiConfig, onSubmit) => {
                 <Typography.Text>{description}</Typography.Text>
                 {input === "textarea" && (
                   <Form.Item name={`${name}-textarea`} rules={rules} noStyle>
-                    <Input.TextArea rows={7} />
+                    <Input.TextArea
+                      rows={7}
+                      placeholder={JSON.stringify(value, null, 2)}
+                      defaultValue={JSON.stringify(value, null, 2)}
+                    />
                   </Form.Item>
                 )}
                 {input === "text" && (
                   <Form.Item name={`${name}-text`} rules={rules} noStyle>
-                    <Input />
+                    <Input placeholder={value} />
                   </Form.Item>
                 )}
                 <Row>
@@ -79,6 +96,14 @@ export const ApiEndpointGenerator = (apiConfig, onSubmit) => {
                       </Button>
                     </Form.Item>
                   </Col>
+                  <Card
+                    title="Response"
+                    bordered={false}
+                    style={{ width: "100%" }}>
+                    <Typography.Text>
+                      <pre> {responseJson}</pre>
+                    </Typography.Text>
+                  </Card>
                 </Row>
               </Form>
             </Collapse.Panel>
